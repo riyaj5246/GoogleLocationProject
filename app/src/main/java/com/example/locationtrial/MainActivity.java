bf220cd1cee835a997c675b4a86a23d36f2b1ebc
Riya Jain <riyaj5246@gmail.com>
2021-10-08 18:59:21 -0500

    internal storage for locations

--- app/src/main/java/com/example/locationtrial/MainActivity.java
+++ app/src/main/java/com/example/locationtrial/MainActivity.java
@@ -9,9 +9,11 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -33,7 +35,9 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -47,12 +51,17 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     MarkerOptions selectedLoc;
     private String fileName = "locationData";
 
+    Button save;
+    Button load;
     Button openFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        save = (Button) findViewById(R.id.saveBtn);
+        load = (Button) findViewById(R.id.loadBtn);
 
         addLocButton = (FloatingActionButton) findViewById(R.id.addLocBtn);
         locNameText = (EditText) findViewById(R.id.locNameTextField);
@@ -185,6 +194,14 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             fragment.setArguments(bundle);
             getSupportFragmentManager().beginTransaction().replace(R.id.main_activity, fragment).commit();
         });
+
+        save.setOnClickListener(view ->{
+            save();
+        });
+
+        load.setOnClickListener(view -> {
+            load(view, googleMap);
+        });
     }
 
     @Override
@@ -221,4 +238,74 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     public ArrayList<Places> getPlaces(){
         return locations;
     }
+
+    public void save()
+    {
+        File file= null;
+        FileOutputStream fileOutputStream = null;
+
+        try {
+            file = getFilesDir();
+            fileOutputStream = openFileOutput("Locations.txt", Context.MODE_PRIVATE); //MODE PRIVATE
+
+            for(Places p:locations){
+                String locData = p.getPlace_name() + ": " + p.getCoordinates().latitude + ", " + p.getCoordinates().longitude + " || ";
+                fileOutputStream.write(locData.getBytes());
+            }
+            Toast.makeText(this, "Successfully Saved \n" + "Path --" + file + "\tCode.txt", Toast.LENGTH_SHORT).show();
+
+            return;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                fileOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void load(View view, GoogleMap googleMap) {
+        locations.clear();
+        ArrayList<Places> newLocations = new ArrayList<>();
+        try {
+            FileInputStream fileInputStream =  openFileInput("Locations.txt");
+            int read = -1;
+            StringBuffer buffer = new StringBuffer();
+            while((read =fileInputStream.read())!= -1){
+                buffer.append((char)read);
+            }
+
+            String newString = buffer.toString();
+            System.out.println("new string: " + newString);
+            while(newString.length() > 0){
+                System.out.println(newString.indexOf(" || "));
+                String location = newString.substring(0, newString.indexOf(" || "));
+                Toast.makeText(this, location, Toast.LENGTH_SHORT).show();
+                locations.add(getLocFromString(location));
+                newString = newString.substring(newString.indexOf(" || ") + 3);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Toast.makeText(this,"Loaded", Toast.LENGTH_SHORT).show();
+        addSavedLocations(googleMap);
+    }
+
+    private Places getLocFromString(String loc){
+        System.out.println(loc);
+        String placeName = loc.substring(0, loc.indexOf(": ") );
+        String placeLat = loc.substring(loc.indexOf(": ") + 2, loc.indexOf(", "));
+        String placeLong = (loc.substring(loc.indexOf(", ") + 2));
+
+        System.out.println(placeName);
+        System.out.println(placeLat);
+        System.out.println(placeLong);
+
+        LatLng x = new LatLng(Double.parseDouble(placeLat), Double.parseDouble(placeLong));
+        return new Places(x, placeName);
+    }
+
 }
